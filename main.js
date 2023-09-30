8374307230b3dc4d5c5c62e90f7f32acc03d7dd5
Brad <brad@learnwebcode.com>
2023-09-30 15:27:33 -0700

    Finished dynamic temperature feature

---
+++ main.js
@@ -0,0 +1,10 @@
+async function start() {
+  const weatherPromise = await fetch("https://api.weather.gov/gridpoints/MFL/110,50/forecast")
+  const weatherData = await weatherPromise.json()
+
+  const ourTemperature = weatherData.properties.periods[0].temperature
+  document.querySelector("#temperature-output").textContent = ourTemperature
+
+}
+
+start()
